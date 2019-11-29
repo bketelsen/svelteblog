0a41bc5baf9b5e4fc0b764f628ac1fb5ea2e6d67
Brian Ketelsen <mail@bjk.fyi>
2019-11-29 21:32:54 +0000

    cart

--- src/routes/courses/[slug].svelte
+++ src/routes/courses/[slug].svelte
@@ -49,6 +49,16 @@
   <hr />
 </header>
 <div class="container">
+<button class="snipcart-add-item"
+  data-item-id="starry-night"
+  data-item-price="79.99"
+  data-item-url="/paintings/starry-night"
+  data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+  data-item-image="/assets/images/starry-night.jpg"
+  data-item-name="The Starry Night">
+  Add to cart
+</button>
+
   <article class="content">
     {@html post.html}
   </article>
